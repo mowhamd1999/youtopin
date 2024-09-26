ba60e1ba51d95e820d3582d1cdef8dc6b8ca8267
mowhamd1999 <mohammaderfansadeghiani@gmail.com>
2024-09-26 23:54:09 +0330

    working on login component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,27 +2,38 @@ import React, { useState } from "react";
 import style from "./login.module.css";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 const Login = () => {
   const [isFocusedUsername, setIsFocusedUsername] = useState(false);
   const [isFocusedPassword, setIsFocusedPassword] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+
+  // مدیریت خطاهای رمز عبور
+  const validatePassword = () => {
+    if (passwordValue === "") {
+      setPasswordError("نمی‌تواند خالی باشد");
+    } else if (passwordValue.length < 6) {
+      setPasswordError("حداقل شش کاراکتر");
+    } else {
+      setPasswordError("");
+    }
+  };
 
   return (
     <div className={style.container}>
       <h4 className={style.h4}>
-        لطفا شماره موبایل یا نام کاربری و رمز عبور خود را وارد کنید
+        لطفا نام کاربری و رمز عبور خود را وارد کنید
       </h4>
-
-      {/* فیلد نام کاربری یا شماره موبایل */}
       <div
         className={`${style.input_container} ${
           isFocusedUsername || inputValue ? style.focused : ""
         }`}
       >
         <label className={style.floating_label}>
-          نام کاربری یا شماره موبایل
+          نام کاربری  
         </label>
         <input
           type="text"
@@ -32,8 +43,6 @@ const Login = () => {
           onChange={(e) => setInputValue(e.target.value)}
         />
       </div>
-
-      {/* فیلد رمز عبور */}
       <div
         className={`${style.input_container} ${
           isFocusedPassword || passwordValue ? style.focused : ""
@@ -41,11 +50,17 @@ const Login = () => {
       >
         <label className={style.floating_label}>رمز عبور</label>
         <input
-          type={showPassword ? "text" : "password"} // تغییر نوع input
+          type={showPassword ? "text" : "password"}
           onFocus={() => setIsFocusedPassword(true)}
-          onBlur={() => setIsFocusedPassword(false)}
+          onBlur={() => {
+            setIsFocusedPassword(false);
+            validatePassword();
+          }}
           value={passwordValue}
           onChange={(e) => setPasswordValue(e.target.value)}
+          className={`${
+            passwordError !== "" ? style.withError : style.withoutError
+          }`}
         />
         <span
           className={style.eye_icon}
@@ -53,6 +68,21 @@ const Login = () => {
         >
           {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
         </span>
+      </div>
+      <div className={style.error_div}>
+        {passwordError && <span className={style.error}>{passwordError}</span>}
+      </div>
+      <div className={style.div_btn}>
+        <button className={style.btn}>ورود</button>
+      </div>
+      <div className={style.p_div}>
+        <p className={style.p}>
+          اگر قبلا اکانت نساخته‌اید برای{" "}
+          <Link to="/singup" className={style.link}>
+            <span className={style.span}> ثبت نام </span>
+          </Link>
+          کلیک کنید
+        </p>
       </div>
     </div>
   );
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,24 +1,63 @@
 import React, { useState } from "react";
 import style from "./singup.module.css";
+import { useFormik } from "formik";
 const Signup = () => {
   const [isFocused, setIsFocused] = useState(false);
-  const [inputValue, setInputValue] = useState("");
+  const [nameValue, setNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [isFocusedUsername, setIsFocusedNumber] = useState(false);
+  const [isFocusedEmail, setIsFocusedEmail] = useState(false);
+  const [numberError, setNumberError] = useState("");
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      avatar: "https://i.postimg.cc/ZRWVFq4z/man.png",
+    },
+  });
+
   return (
     <div className={style.container}>
-      <div
-        className={`${style.input_container} ${
-          isFocused || inputValue ? `${style.focused}` : ""
-        }`}
-      >
-        <label className={style.floating_label}>نام کاربری یا شماره موبایل</label>
-        <input
-          type="text"
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-      </div>
+      <h4 className={style.h4}>اطلاعات خود را وارد کنید</h4>
+      <form onSubmit={formik.handleSubmit}>
+        <div
+          className={`${style.input_container} ${
+            isFocusedUsername || nameValue ? style.focused : ""
+          }`}
+        >
+          <label className={style.floating_label}>نام و نام خانوادگی</label>
+          <input
+            type="text"
+            onFocus={() => setIsFocusedNumber(true)}
+            onBlur={() => setIsFocusedNumber(false)}
+            {...formik.getFieldProps("name")}
+            className={`${
+              numberError !== "" ? style.withError : style.withoutError
+            }`}
+          />
+        </div>
+        <div className={style.error_div}>
+          {/* {numberError && <span className={style.error}>{numberError}</span>} */}
+        </div>
+        <div
+          className={`${style.input_container} ${
+            isFocusedEmail || emailValue ? style.focused : ""
+          }`}
+        >
+          <label className={style.floating_label}>ایمیل</label>
+          <input
+            type="email"
+            onFocus={() => setIsFocusedEmail(true)}
+            onBlur={() => setIsFocusedEmail(false)}
+            {...formik.getFieldProps("email")}
+            className={`${
+              numberError !== "" ? style.withError : style.withoutError
+            }`}
+          />
+        </div>
+      </form>
     </div>
   );
 };
