c50f36c370c099ac41317abd27f4f0ebd70282a3
mowhamd1999 <mohammaderfansadeghiani@gmail.com>
2024-10-08 22:37:10 +0330

    working on cart page

--- src/components/cart_tag/cart_tag_box/Cart_tag_box.jsx
+++ src/components/cart_tag/cart_tag_box/Cart_tag_box.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import style from './cart_tag_box.module.css'
-const Cart_tag_box = () => {
+const Cart_tag_box = ({details}) => {
   return (
     <div className={style.container}>
-      s
+      <h4 className={style.h4}>سبد خرید</h4>
+      {details.products.map(item => (
+        <p>s</p>
+      ))}
     </div>
   )
 }
